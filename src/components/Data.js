0264a3a97852e4cacad70052beee45d2eb184b47
Thinh Duong <duongphucthinh910@gmail.com>
2020-12-01 17:22:30 +0700

    Add images links

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,55 +3,55 @@ export const data = [
     id : 1,
     name: "Trần Hồng Văn",
     age: 20,
-    image: 'https://i.ibb.co/8xyNtcL/79018980-1426488010852156-8644876857327484928-n.jpg'
+    image: "https://i.ibb.co/ZNCMPXF/goku2.jpg"
   },
   {
     id: 2,
     name: "Trần Phạm Hoài Tiến",
     age: 22,
-    image: ''
+    image: "https://i.ibb.co/pK525yZ/goku1.jpg"
   },
   {
     id: 3,
     name:"Lê Văn Thạch",
     age: 22,
-    image: '',
+    image: "https://i.ibb.co/5n6mzv7/photo-1444703686981-a3abbc4d4fe3.jpg"
 
   },
   {
     id: 4,
     name: 'Hà Ngủ Tân',
     age: 21,
-    image: "",
+    image:"https://i.ibb.co/ZSJqkTp/photo-1504607704864-1b30a4298b62.jpg" 
   },
   {
     id: 5,
     name: "Nguyễn Thị Trinh",
     age: 20,
-    image: "",
+    image: "https://i.ibb.co/ZNCMPXF/goku2.jpg",
   },
   {
     id: 6,
     name: "Hồ Thị Trang",
     age: 19,
-    image: "",
+    image: "https://i.ibb.co/5n6mzv7/photo-1444703686981-a3abbc4d4fe3.jpg",
   },
   {
     id: 7,
     name: "Võ Văn Tuấn",
     age: 23,
-    image: "",
+    image: "https://i.ibb.co/pK525yZ/goku1.jpg",
   },
   {
     id: 8,
     name: 'Phạm Thừa Tiểu Thành',
     age: 24,
-    image: "",
+    image: "https://i.ibb.co/ZNCMPXF/goku2.jpg",
   },
   {
     id: 9,
     name: "Hoàng Thị Trinh",
     age: 22,
-    image: "",
+    image: "https://i.ibb.co/5n6mzv7/photo-1444703686981-a3abbc4d4fe3.jpg",
   }
 ]
